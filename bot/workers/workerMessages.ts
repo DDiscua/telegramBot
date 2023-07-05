09608cc6ff7abb4f22172a6a510b3b23ca132a49
DDiscua <ddiscua.dragon@gmail.com>
2023-07-04 19:05:21 -0600

    remove workers

--- bot/workers/workerMessages.ts
+++
@@ -1,9 +0,0 @@
-import { BotWorker } from '@grammyjs/runner';
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-//Env vars
-const BOT_TOKEN = process.env.BOT_TOKEN || '';
-
-// Create a new bot worker.
-const bot = new BotWorker(BOT_TOKEN);
