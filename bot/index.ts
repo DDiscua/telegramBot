43f490f6afafea4f5a6f185e59252f9649f82388
DDiscua <ddiscua.dragon@gmail.com>
2023-07-04 19:05:19 -0600

    remove workers

--- bot/index.ts
+++ bot/index.ts
@@ -1,18 +1,9 @@
-import { run, distribute } from '@grammyjs/runner';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { run } from '@grammyjs/runner';
 import { bot } from './bot';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const runBot = () => {
     if (!bot.isInited()) {
         console.log('BOT NOT INITIATED');
-        const pathToWorkers = `${__dirname}/workers`;
-        //Distribute
-        //  bot.use(distribute(pathToWorkers + '/workerMessages.ts'));
-
         run(bot);
     }
 };
